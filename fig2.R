f6147750cb65ce8198bed50a111cd37222686f1d
MilliLin <126165687+MilliLin@users.noreply.github.com>
2023-08-24 16:25:20 +0100

    Update fig2.R

--- fig2.R
+++ fig2.R
@@ -1,4 +1,3 @@
-#FOR FIGURE ONLY
 
 library(dplyr)
 library(ggplot2)
@@ -11,15 +10,10 @@ forr_data_frame <- as.data.frame(forr)
 forr2<- forr_data_frame[c(TRUE, FALSE, FALSE, FALSE),]
 forr2<- forr2 %>% filter(Reference_tree_size==500)
 
-
-# Assuming forr1 is a tibble containing the necessary columns
-# Create a custom order for the Levels variable
 custom_order <- c("Order", "Infraorder", "Superfamily", "Family", "Subfamily")
 
-# Convert the Levels variable to a factor with custom order
 forr2$Taxonomic_levels <- factor(forr2$Taxonomic_levels, levels = custom_order)
 
-# Create the ggplot with the custom order
 summary_forr2 <- forr2 %>%
   group_by(Phylogenetic_methods, Reference_tree_size, Taxonomic_levels) %>%
   summarise(
@@ -40,11 +34,11 @@ fig2<-ggplot(summary_forr2, aes(x = Phylogenetic_methods, y = Mean, fill = Taxon
   geom_text(aes(label = sprintf("%.1f", Mean)), 
             position = position_dodge(width = 0.9), 
             vjust = -0.5,
-            color = "#808080",    # Change the text color to black
+            color = "#808080", 
             size = 4) +
   theme(
-    panel.grid.minor = element_blank(),# 去掉主要网格线
-    panel.border = element_blank()  # 去掉边框线
+    panel.grid.minor = element_blank(),
+    panel.border = element_blank() 
   )
 
 fig2
